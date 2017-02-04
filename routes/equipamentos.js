864ba2157718ae80b7bc93b4fdecc4ff2ef83761
Maurício Jr <mauricio@mjunior.com.br>
2017-02-04 02:14:56 -0200

    V1.0

--- routes/equipamentos.js
+++ routes/equipamentos.js
@@ -9,22 +9,46 @@ admin.initializeApp({
 });
 
 var db = admin.database();
-var ref = db.ref("equipamentos");
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
+  // As an admin, the app has access to read and write all data, regardless of Security Rules
+  var ref = db.ref("equipamentos");
   // Attach an asynchronous callback to read the data at our posts reference
   ref.on("value", function(snapshot) {
+    res.render('index', { colheitadeiras: snapshot.val() });
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
+});
+
+//request.params.id
+
+//harverst/S540/colher_soja
+router.get('/:banco/:modelo/:pergunta', function(req, res, next) {
+
+  var modelo = req.params.modelo;
+  var pergunta = req.params.pergunta;
+  var banco = req.params.banco
+  var url = ""
+
+  if(banco && modelo && pergunta){
+    //harverst/S540/colher_soja
+    url = banco+"/"+modelo+"/"+pergunta+""
+  }
+
+  // As an admin, the app has access to read and write all data, regardless of Security Rules
+  var ref = db.ref(url);
+  // Attach an asynchronous callback to read the data at our posts reference
+  ref.on("value", function(snapshot) {
+    console.log(snapshot.val());
     res.send(snapshot.val());
   }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
 
-  // ref.once("value", function(snapshot) {
-  //   console.log(snapshot.val());
-  //   res.send(snapshot.val());
-  // });
-
 });
+
+
 
 module.exports = router;
